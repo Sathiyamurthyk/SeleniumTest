1bd6b05ce7cd4a81284cbf10dab9417b22b70564
sathiyamoorthi ssc <smoorthy669@gmail.com>
2022-09-22 08:17:42 -0700

    saving project

---
+++ MyPhone/src/org/phone/PhoneInfo.java
@@ -0,0 +1,19 @@
+package org.phone;
+
+public class PhoneInfo {
+public static void phone_name()
+{
+	System.out.println("phone:samsung");
+}
+public static void camera() {
+	System.out.println("camera:12mp");
+}
+public static void storage() {
+	System.out.println("storage:64gb");
+}
+public static void main(String[] args) {
+	PhoneInfo.phone_name();
+	PhoneInfo.camera();
+	PhoneInfo.storage();
+}
+}
